8dfb36e289dfeb438970513fef32a8863d37193e
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-05 03:06:28 +0200

    Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    arr = []
+    for s in range(n):
+        if s > 0:
+            copy_arr = small_arr
+        small_arr = []
+        for d in range(s + 1):
+            if d == 0:
+                small_arr.append(1)
+            elif s == 1:
+                small_arr.append(1)
+            elif d < s:
+                num = copy_arr[d - 1] + copy_arr[d]
+                small_arr.append(num)
+            else:
+                small_arr.append(1)
+        arr.append(small_arr)
+    return arr
