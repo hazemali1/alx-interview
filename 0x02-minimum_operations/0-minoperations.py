d54e8d5894b1235095a7664314355554795a03f1
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-22 00:46:51 +0200

    min operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,11 +4,13 @@ def minOperations(n):
     """min operations"""
     if n <= 1:
         return 0
-    f = 0
+    f = 2
     l = 0
-    for i in range(2, n):
-        if n % i == 0:
-            if f == 0 or (f - l > 0 and i - n / i >= 0 and f - l > i - n / i):
-                l = i
-                f = n / i
-    return int(f + l)
+    while f <= n:
+        if n % f == 0:
+            print(f)
+            l += f
+            n = n / f
+        else:
+            f += 1
+    return l
