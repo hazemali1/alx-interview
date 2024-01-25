a818caef2fb65855642942a1ea23d10849f19cd4
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-25 15:45:00 +0200

    stdin

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -16,7 +16,7 @@ try:
         status = s[-2]
         if status in l.keys():
             l[status] += 1
-        elif status in [200, 301, 400, 401, 403, 404, 405, 500]:
+        elif int(status) in [200, 301, 400, 401, 403, 404, 405, 500]:
             l[status] = 1
         if count == 10:
             count = 0
