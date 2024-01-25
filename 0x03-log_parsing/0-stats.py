02db14acbcde0f1ec37cefa720d12cfb2ffc82ee
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-25 15:17:41 +0200

    stdin

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,6 +4,8 @@ import sys
 
 
 count = 0
+summ = 0 
+l = {}
 
 
 try:
@@ -11,9 +13,17 @@ try:
         s = line.split()
         print(s)
         count += 1
+        summ += s[-1]
+        status = s[-2]
+        if status in l.keys():
+            l[status] += 1
+        else:
+            l[status] = 1
         if count == 10:
             count = 0
-            print("File size: <total size>")
+            print("File size: {}".format(summ))
+            for k, v in l.items():
+                print("{}: {}".format(k, v))
 
 except KeyboardInterrupt:
-    print("File size: <total size>")
+    print("File size: {}".format(summ))
