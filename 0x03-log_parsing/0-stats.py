a91a3f403a2662f14d948983488719f72f4aff99
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-25 16:28:38 +0200

    stdin

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,15 +4,6 @@ import sys
 
 
 def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
     print("File size: {}".format(total_file_size))
     for key, val in sorted(dict_sc.items()):
         if val != 0:
