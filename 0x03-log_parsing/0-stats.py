d1b355fc0d34efd1d12b25e7e78a42f1eeec617f
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-25 16:03:59 +0200

    stdin

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,33 @@
 #!/usr/bin/python3
-
+"""read stdin"""
 import sys
 
 
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
+if __name__ == '__main__':
+    """main"""
+    count = 0
+    summ = 0
+    li = {}
 
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+    try:
+        for line in sys.stdin:
+            s = line.split()
+            count += 1
+            summ += int(s[-1])
+            status = s[-2]
+            if status in li.keys():
+                li[status] += 1
+            elif int(status) in [200, 301, 400, 401, 403, 404, 405, 500]:
+                li[status] = 1
+            if count == 10:
+                count = 0
+                print("File size: {}".format(summ))
+                ll = {key: li[key] for key in sorted(li)}
+                for k, v in ll.items():
+                    print("{}: {}".format(k, v))
 
-
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
-
-try:
-    for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
-
-        if len(parsed_line) > 2:
-            counter += 1
-
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
-
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
-finally:
-    print_msg(dict_sc, total_file_size)
+    except KeyboardInterrupt:
+        print("File size: {}".format(summ))
+        ll = {key: li[key] for key in sorted(li)}
+        for k, v in ll.items():
+            print("{}: {}".format(k, v))
