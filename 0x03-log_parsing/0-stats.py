db67607b2d0aa92fc64750c8ca4526f44cd6a021
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-25 14:50:07 +0200

    stdin

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """read stdin"""
+import sys
 
 
 try:
     while True:
-        line = input()
-        print(line)
+        for line in sys.stdin:
+            print(line)
 
 except EOFError:
     print("End of input.")
