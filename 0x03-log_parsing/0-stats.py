2d07581b0a9e2b41d8dbebb3ebd05390e53ac285
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-25 15:58:02 +0200

    stdin

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,32 +1,39 @@
 #!/usr/bin/python3
-"""read stdin"""
+"""
+Log parsing
+"""
+
 import sys
 
+if __name__ == '__main__':
 
-count = 0
-summ = 0
-li = {}
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-
-try:
-    for line in sys.stdin:
-        s = line.split()
-        count += 1
-        summ += int(s[-1])
-        status = s[-2]
-        if status in li.keys():
-            li[status] += 1
-        elif int(status) in [200, 301, 400, 401, 403, 404, 405, 500]:
-            li[status] = 1
-        if count == 10:
-            count = 0
-            print("File size: {}".format(summ))
-            ll = {key: li[key] for key in sorted(li)}
-            for k, v in ll.items():
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
                 print("{}: {}".format(k, v))
 
-except KeyboardInterrupt:
-    print("File size: {}".format(summ))
-    ll = {key: li[key] for key in sorted(li)}
-    for k, v in ll.items():
-        print("{}: {}".format(k, v))
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, filesize)
+        raise
