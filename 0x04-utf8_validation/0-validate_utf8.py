79075c6477198f69369580a1e2394566a90bb1d6
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-01 15:51:20 +0200

    utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,21 +3,21 @@
 
 
 def validUTF8(data):
-    i = 0
-    while i < len(data):
-        byte = data[i]
-        if byte & 0b10000000 == 0:  # 1-byte character
-            i += 1
-        elif byte & 0b11100000 == 0b11000000:  # 2-byte character
-            i += 2
-        elif byte & 0b11110000 == 0b11100000:  # 3-byte character
-            i += 3
-        elif byte & 0b11111000 == 0b11110000:  # 4-byte character
-            i += 4
+    """valid UTF8"""
+
+    j = 0
+    while j < len(data):
+        i = data[j]
+        if format(i, '08b')[0] == '0':
+            j += 1
+        elif format(i, '08b')[0:3]== '110':
+            j += 2
+        elif format(i, '08b')[0:4] == '1110':
+            j += 3
+        elif format(i, '08b')[0:5] == '11110':
+            j += 4
         else:
             return False
-
-        if i > len(data) or byte > 255:
+        if j > len(data) or i > 255:
             return False
-
     return True
