634d2640aef58780634dcffc4a4c376d3cd0fb54
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-01 18:54:44 +0200

    utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,9 +13,9 @@ def validUTF8(data):
             s += 1
         elif format(i, '08b')[0:3] == '110':
             s += 2
-            w = 1
         elif format(i, '08b')[0:4] == '1110':
             s += 3
+            w = 1
         elif format(i, '08b')[0:5] == '11110':
             s += 4
         else:
