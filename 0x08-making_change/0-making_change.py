b90b3f1157b74257cf2a7d910fcc7d61710e3d7d
hazemali1 <hazemali1343@gmail.com>
2024-03-17 15:12:37 +0000

    make change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""no modules """
+
+
+def makeChange(coins, total):
+    """make change function"""
+    if total <= 0:
+        return 0
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
